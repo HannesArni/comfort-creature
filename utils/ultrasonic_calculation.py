5274b7aa7ab8b759088f4fdf6e1fa15fddcc5b44
Hannes Arni <hannesarni@gmail.com>
2025-10-24 17:27:57 +0000

    feat: comment some initial thoughts on ultrasonic calculation

--- utils/ultrasonic_calculation.py
+++ utils/ultrasonic_calculation.py
@@ -2,5 +2,10 @@ from utils import Coordinate
 
 
 def ultrasonic_calculation(target: Coordinate):
-    # Placeholder for ultrasonic calculation logic
-    print(f"Calculating ultrasonic data for target at ({target.x}, {target.y})")
+    # TODO: Based on ultrasonic data, estimate objects around something like this:
+    # get_ultrasonic_hit_points(): List[Coordinate]
+    # Interpolate between those points to figure out where we can navigate, fx. by making a bounding box the size of the chair
+    # around the translation vector we're trying to go to
+
+    # By default, we'll go to the same target we receive, if we don't determine any problems
+    return target
