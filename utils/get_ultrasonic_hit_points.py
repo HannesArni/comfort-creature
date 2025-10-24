1786dfd086c6895df0f7f7bc53a7fee27b08d7e2
Hannes Arni <hannesarni@gmail.com>
2025-10-24 18:01:05 +0000

    feat: calculate ultrasonic hit points in a local coordinate systems

---
+++ utils/get_ultrasonic_hit_points.py
@@ -0,0 +1,35 @@
+import math
+from dataclasses import dataclass
+from utils import LocalPose, LocalCoordinate
+
+
+@dataclass
+class UltrasonicSensor:
+    """
+    Where is the sensor located on the chair?
+    """
+    pose: LocalPose
+    name: str = "Ultrasonic Sensor"
+
+sensors = [
+    UltrasonicSensor(LocalPose(LocalCoordinate(30, 30), math.radians(45)), 'Front-Left'),
+    UltrasonicSensor(LocalPose(LocalCoordinate(30, 30), 0), 'Front'),
+    UltrasonicSensor(LocalPose(LocalCoordinate(-30, 30), math.radians(365-45)), 'Front-Right'),
+]
+
+def get_ultrasonic_hit_points() -> list[LocalCoordinate]:
+    hit_points = []
+    for sensor in sensors:
+        distance_reading = 100  # TODO: Replace with actual reading from sensor, let's work with 100 cm for now
+
+        # You remember pythagoras from school, right?
+        x_translation = distance_reading * math.sin(sensor.pose.heading)
+        y_translation = distance_reading * math.cos(sensor.pose.heading)
+
+        hit_point = sensor.pose.position.move(x_translation, y_translation)
+        hit_points.append(hit_point)
+        print(f"Sensor {sensor.name} at position {sensor.pose.position} with heading {sensor.pose.heading} reads distance {distance_reading} cm, hit point at {hit_point}")
+
+    return hit_points
+
+
